84a7f71598c7e0813b3fbf6b2ddd7f185d77627d
sarvarabduganiyev <sarvarabduganiyev636@gmail.com>
2022-07-02 08:27:35 +0500

    last page some finished

--- js/app.js
+++ js/app.js
@@ -39,3 +39,17 @@ values.forEach(function (value, i, values) {
 
 elm.remove();
 // range js
+
+function openCity(evt, cityName = "London") {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(cityName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
